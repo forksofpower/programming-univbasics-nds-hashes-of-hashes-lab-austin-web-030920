37ba3643aa0aa58f7f80d78fe53f089e65fffd81
Patrick Jones <patrickjones.pmj@gmail.com>
2020-03-04 06:35:06 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -43,11 +43,11 @@ def naming_system
   # HoH all the way to Species!
 
   taxonomy = {
-    label: "Kingdom",
-    sub_category: {
-      label: "Phylum",
-      sub_category: {
-        label: "Class"
+    :label "Kingdom",
+    :sub_category {
+      :label "Phylum",
+      :sub_category {
+        :label "Class"
       }
     }
   }
